b0d84198c544a1b6e68c0f837339f8bb672105ba
Harshavardhan Kona <harshavardhan.kona06@icloud.com>
2025-01-02 18:54:48 -0500

    Add font family and color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,32 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bg-primary-dark': 'var(--bg-primary-dark)',
+        'bg-secondary-dark': 'var(--bg-secondary-dark)',
+        'bg-tertiary-dark': 'var(--bg-tertiary-dark)',
+
+        'text-primary-dark': 'var(--text-primary-dark)',
+        'text-secondary-dark': 'var(--text-secondary-dark)',
+
+        'border-dark': 'var(--border-dark)',
+
+        'electric-blue-dark': 'var(--electric-blue-dark)',
+        'purple-dark': 'var(--purple-dark)',
+        'sky-dark': 'var(--sky-dark)',
+        'teal-dark': 'var(--teal-dark)',
+
+        'purple-hover-dark': 'var(--purple-hover-dark)',
+        'sky-hover-dark': 'var(--sky-hover-dark)',
+        'teal-hover-dark': 'var(--teal-hover-dark)',
+      },
+      fontFamily: {
+        'montserrat': 'var(--font-montserrat)',
+        'firaCode': 'var(--font-fira-code)',
+        'ebGaramond': 'var(--font-ebGaramond)',
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
